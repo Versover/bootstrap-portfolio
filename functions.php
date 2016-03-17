34817bd9b9bf97a1037ce5dacfe7ee9607a33022
wizard2014 <wizard2014@ya.ru>
2016-03-18 00:21:57 +0200

    add theme functions and icons

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * functions.php
+ *
+ * The themes function's
+ */
+
+/**
+ * 1. Constants
+ */
+define( 'THEMEROOT', get_stylesheet_directory_uri() );
+define( 'IMAGES', THEMEROOT . '/img' );
+define( 'JS', THEMEROOT . '/js' );
+?>
--- header.php
+++ header.php
@@ -14,14 +14,22 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Bootstrap Portfolio Build | Home</title>
-    <meta name="description" content="">
+    <title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+
+    <!-- Favicon & Apple Touch Icon -->
+    <?php
+        $favicon = IMAGES . '/icons/favicon.ico';
+        $touchicon = IMAGES . '/icons/apple-touch-icon-precomposed.png';
+    ?>
+
+    <link rel="shortcut icon" href="<?php echo $favicon; ?>">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo $touchicon; ?>" sizes="152x152">
+
+    <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <!--[if lt IE 7]>
 <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
@@ -32,13 +40,13 @@
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only"><?php _e( 'Toggle navigation', 'versover' ); ?></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">
-                Ice Breaker
+            <a class="navbar-brand" href="<?php esc_url( home_url('/') ); ?>">
+                <?php bloginfo( 'name' );?>
             </a>
         </div>
 
