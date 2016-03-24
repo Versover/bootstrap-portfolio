e0d24a727c41c11856b8d67d6ed3d65352900cf9
wizard2014 <wizard2014@ya.ru>
2016-03-24 23:43:34 +0200

    add content page

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ define( 'IMAGES', THEMEROOT . '/img' );
 define( 'JS', THEMEROOT . '/js' );
 
 /**
- * 1. Theme Setup
+ * 2. Theme Setup
  */
 if ( ! function_exists( 'versover_theme_setup' ) ) {
     function versover_theme_setup() {
@@ -34,4 +34,20 @@ if ( ! function_exists( 'versover_theme_setup' ) ) {
     }
 
     add_action( 'after_setup_theme',  'versover_theme_setup');
+}
+
+/**
+ * 3. Get post meta
+ */
+if ( ! function_exists( 'versover_post_meta' ) ) {
+    function versover_post_meta() {
+        if ( get_post_type() === 'post' ) {
+            /* Post author */
+            echo '<p class="post-meta">';
+            _e( 'by ', 'versover' );
+            printf( '<a href="%1$s">%2$s</a>', esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ), get_the_author() );
+            _e( ' on ', 'versover' );
+            echo '<span>' . get_the_date() . '</span></p>';
+        }
+    }
 }
--- index.php
+++ index.php
@@ -10,3 +10,38 @@
 /* Load header.php */
 get_header();
 ?>
+
+<div class="container-fluid text-center">
+	<div class="jumbotron">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<h1><?php _e('My thoughts on and off the web', 'versover'); ?></h1>
+				<p class="lead">
+					<?php _e('Web-design, development, and parent-teacher conferences.', 'versover'); ?>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="page-blog container-fluid">
+	<div class="row">
+
+		<aside class="sidebar col-md-3 col-md-offset-1 col-md-push-8">
+			sidebar here
+		</aside>
+
+		<div class="posts col-md-8 col-md-pull-4">
+			<div class="row">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php endwhile; ?>
+
+				<?php else : ?>
+					<?php get_template_part( 'content', 'none' ); ?>
+				<?php endif; ?>
+			</div>
+		</div>
+
+	</div>
+</div>
